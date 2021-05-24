9563c5186738b2659cb4bc0214e6c5c1d2910419
PoriyaAbdollahi <poriyaa58@gmail.com>
2021-05-24 16:16:30 +0430

    Initial commit

---
+++ app/src/main/java/com/poriyaabdollahi/karam/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.poriyaabdollahi.karam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
